df6fdce7ead64ca4523e58a389e5b0429f3714c2
HerokeyVN <70248685+HerokeyVN@users.noreply.github.com>
2022-11-07 20:15:45 +0700

    Update defaultConfig.js

--- core/util/defaultConfig.js
+++ core/util/defaultConfig.js
@@ -22,7 +22,6 @@ function core() {
 	return{
     	"main_bot": {
     		"consoleColor": "32",
-    		//https://upload.wikimedia.org/wikipedia/commons/3/34/ANSI_sample_program_output.png
     		"dataSaveTime": "5",
     		"developMode": false,
     		"toggleLog": true,
